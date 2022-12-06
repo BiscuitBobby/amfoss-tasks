5e39a053b1a66e8b0cf6c85595db44daa43d40ca
BiscuitBobby <87699062+BiscuitBobby@users.noreply.github.com>
2022-12-06 19:30:34 +0530

    task 8

---
+++ task-8/index.js
@@ -0,0 +1,37 @@
+var music = new Audio();
+
+    function w() {
+    music.pause();
+    music = new Audio('sounds/tom-1.mp3');
+    music.play();
+	  }
+      function a() {
+    music.pause();
+    music = new Audio('sounds/tom-2.mp3');
+    music.play();
+      }
+      function s() {
+    music.pause();
+    music = new Audio('sounds/tom-3.mp3');
+    music.play();
+      }
+      function d() {
+    music.pause();
+    music = new Audio('sounds/tom-4.mp3');
+    music.play();
+      }
+      function j() {
+    music.pause();
+    music = new Audio('sounds/snare.mp3');
+    music.play();
+      }
+      function k() {
+    music.pause();
+    music = new Audio('sounds/kick-bass.mp3');
+    music.play();
+      }
+      function l() {
+    music.pause();
+    music = new Audio('sounds/crash.mp3');
+    music.play();
+      }
